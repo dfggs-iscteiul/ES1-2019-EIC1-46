a484b45e9d173e88deef78228ac9d1ec1db800e8
JoaoMacedo <jmmos1@iscte-iul.pt>
2019-10-13 23:08:00 +0100

    Joao's Class

---
+++ ProjetoESI/src/first/Joao_Class.java
@@ -0,0 +1,9 @@
+package first;
+
+public class Joao_Class {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("João Martins de Macedo");
+	}
+
+}
