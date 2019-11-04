a26e9bf0afb2783f487a319f927cfcbb138c23d6
Daniel Gonçalves <dfggs@iscte-iul.pt>
2019-11-04 15:19:01 +0000

    Sprint #1 - Dados: 1 - Eliminação da classe inicial "Daniel's Class"; 2 - Criação do Package "code" para separar a GUI do código funcional; 3 - Criação da classe "main"; 4 - Criação da estrutura de dados "Data_Entry" para utilização na altura da importação da folha excel.

--- ProjetoESI/src/code/main.java
+++ ProjetoESI/src/code/main.java
@@ -1,10 +1,10 @@
-package first;
+package code;
 
-public class HelloWorld {
+public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+
 	}
 
 }
