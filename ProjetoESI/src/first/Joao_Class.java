88926f9b6a8b12dede1310d8142a23f0017eeb9b
gavpf-iscteiul <gavpf@iscte-iul.pt>
2019-11-05 14:59:34 +0000

    Converti o projeto para maven

--- ProjetoESI/src/first/Joao_Class.java
+++
@@ -1,9 +0,0 @@
-package first;
-
-public class Joao_Class {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("João Martins de Macedo");
-	}
-
-}
