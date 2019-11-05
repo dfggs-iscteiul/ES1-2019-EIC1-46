a412c2608d0b89ace7aaace98b660bed5b5ddda1
gavpf-iscteiul <gavpf@iscte-iul.pt>
2019-11-05 17:41:01 +0000

    Ler dados do excel

--- ProjetoESI/src/code/LerDadosExcel.java
+++ ProjetoESI/src/code/LerDadosExcel.java
@@ -4,15 +4,56 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class LerDadosExcel {
 
 	public static void main(String[] args) {
+		
+		File excelFile = new File("Long-Method.xlsx");
+		FileInputStream fis=null;
+		try {
+			fis = new FileInputStream(excelFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		
+		Iterator<Row> rowIt = sheet.iterator();
+		
+		while(rowIt.hasNext()) {
+			Row row = rowIt.next();
+			
+			Iterator<Cell> cellIterator = row.cellIterator();
+			
+			while(cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+				System.out.println(cell.toString() + ";");
+			}
+		}
+		
+		try {
+			fis.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			workbook.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 }
