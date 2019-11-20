0780d3dc321bcb1a3b66f244821ab9fc9b51b4dd
fmmcz-iscteiul <fmmcz@iscte-iul.pt>
2019-11-20 21:24:38 +0000

    Update GUITest.

--- ProjetoESI/src/gui/GUITest.java
+++ ProjetoESI/src/gui/GUITest.java
@@ -1,8 +1,25 @@
 package gui;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class GUITest {
 
-	public static void main(String[] args) {
+	private JFrame frame;
 
+	public void createandShowGUI() {
+		frame = new JFrame();
+		frame.setTitle("Projeto ES1");
+		frame.setLayout(new GridBagLayout());
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		GUITest gui = new GUITest();
+		gui.createandShowGUI();
 	}
 
 }
