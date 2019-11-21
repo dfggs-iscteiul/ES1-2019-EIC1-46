aa45acb445e246d3971191016c9262ee20afc6eb
fmmcz-iscteiul <fmmcz@iscte-iul.pt>
2019-11-21 15:06:30 +0000

    Troca de GUI.

--- ProjetoESI/src/code/Main.java
+++ ProjetoESI/src/code/Main.java
@@ -1,6 +1,5 @@
 package code;
 
-import gui.GUITest;
 import gui.GUI;
 /**
  * Main class, it runs the program.
@@ -15,7 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GUI window = new GUI();
-		window.createAndShowGUI();
+		window.createandShowGUI();
 		System.out.println("Janela aberta!");
 	}
 
