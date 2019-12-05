818d59c16df2825a3165403f6d64d37d37dbf3e0
talfs-iscteiul <talfs@iscte-iul.pt>
2019-12-05 00:02:46 +0000

    JavaDoc da classe ExcelAccuracy

--- ProjetoESI/src/code/ExcelAccuracy.java
+++ ProjetoESI/src/code/ExcelAccuracy.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * 
+ * Calculates the long method checking tools accuracy.
+ * 
+ *
+ */
 public class ExcelAccuracy {
 
 	private ArrayList<DataEntry> entradas;
@@ -36,10 +42,19 @@ public class ExcelAccuracy {
 		this.entryAccuracy = (entradaCerta * 100 / entradas.size());
 	}
 
+	
+	/**
+	 * 
+	 * @return average percentage of the iPlasma and PMD accuracy.
+	 */
 	public double getAverageAccuracy() {
 		return (iPlasmaAccuracy + PMDAccuracy) / 2;
 	}
 
+	/**
+	 *  
+	 * @return percentage of the lines 100% correct. Which means that the PMD and the iPlasma values are the same that the isLongMethod.
+	 */
 	public double getEntryAccuracy() {
 		return entryAccuracy;
 	}
